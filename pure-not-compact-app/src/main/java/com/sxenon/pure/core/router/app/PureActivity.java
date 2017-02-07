6d6276f59dff7ec1d14ce2563e37eba12bae3d1c
sxenon <suiisagenius@gmail.com>
2017-02-07 22:03:38 +0800

    依赖库更新

--- pure-not-compact-app/src/main/java/com/sxenon/pure/core/router/app/PureActivity.java
+++ pure-not-compact-app/src/main/java/com/sxenon/pure/core/router/app/PureActivity.java
@@ -14,101 +14,71 @@
  * limitations under the License.
  */
 
-package com.sxenon.pure.core.app;
+package com.sxenon.pure.core.router.app;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v13.app.FragmentCompat;
-import android.view.View;
+import android.support.v4.app.ActivityCompat;
 
 import com.sxenon.pure.core.Event;
+import com.sxenon.pure.core.global.GlobalContext;
+import com.sxenon.pure.core.router.IActivity;
 import com.sxenon.pure.core.router.IRouter;
 import com.sxenon.pure.core.router.PureRootPresenter;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
- * 做最纯净的Fragment二次封装
+ * To be the purest wrapper for Activity
+ * Attention:Those activities which call finish in onCreate() should not inherit it.
  * Created by Sui on 2016/11/21.
  */
 
-public abstract class PureFragment<P extends PureRootPresenter> extends Fragment implements IRouter<P> {
-    private List<Event> mSavedEventList;
+public abstract class PureActivity<P extends PureRootPresenter> extends Activity implements IActivity<P> {
     private P mRootPresenter;
-    private boolean mViewCreated;
-    private boolean shouldInitRootPresenter;
-    /**
-     * 真正的对用户可见的状态！！！
-     */
-    private boolean mVisible;
+    private final Set<PureFragment> mVisibleFragmentSet=new HashSet<>();
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        mRootPresenter = bindVisitor();
-        mViewCreated = true;
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mRootPresenter= bindVisitor();
+        mRootPresenter.onCreate(savedInstanceState==null?null:GlobalContext.INSTANCE.savedEventList);
+        GlobalContext.INSTANCE.activityHistoryManager.add(this);
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        initRootPresenterIfNeeded();
-    }
-
-    @Override
-    public void onResume() {
+    protected void onResume() {
         super.onResume();
         mRootPresenter.onResume();
     }
 
     @Override
-    public void onPause() {
+    protected void onPause() {
         super.onPause();
         mRootPresenter.onPause();
     }
 
     @Override
-    public void onStop() {
+    protected void onStop() {
         super.onStop();
         mRootPresenter.onStop();
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
+    protected void onDestroy() {
+        super.onDestroy();
         //noinspection unchecked
         saveEventList(mRootPresenter.getEventForSave());
         mRootPresenter.onDestroy();
-        mViewCreated = false;
-        shouldInitRootPresenter = true;
-    }
-
-    @Override
-    public void setUserVisibleHint(boolean isVisibleToUser) {
-        super.setUserVisibleHint(isVisibleToUser);
-        mVisible = mViewCreated && isVisibleToUser;
-        initRootPresenterIfNeeded();
-        PureActivity activity = (PureActivity) getActivity();
-        if (mVisible) {
-            activity.addToVisibleSet(this);
-        } else {
-            if (activity != null) {
-                activity.removeFromVisibleSet(this);
-            }
-        }
-    }
-
-    private void initRootPresenterIfNeeded() {
-        if (mVisible && shouldInitRootPresenter) {
-            mRootPresenter.onCreate(mSavedEventList);
-            shouldInitRootPresenter = false;
-        }
+        GlobalContext.INSTANCE.activityHistoryManager.remove(this);
     }
 
     @Override
@@ -122,6 +92,12 @@ public abstract class PureFragment<P extends PureRootPresenter> extends Fragment
         return this;
     }
 
+    @NonNull
+    @Override
+    public Context getContext() {
+        return this;
+    }
+
     @Override
     public P getPresenter() {
         return mRootPresenter;
@@ -129,36 +105,45 @@ public abstract class PureFragment<P extends PureRootPresenter> extends Fragment
 
     @Override
     public void saveEventList(List<Event> eventList) {
-        mSavedEventList = eventList;
+        GlobalContext.INSTANCE.savedEventList=eventList;
     }
 
     @Override
     public Activity getActivityCompact() {
-        return getActivity();
+        return this;
     }
 
     @Override
     public void requestPermissionsCompact(@NonNull String[] permissions, int requestCode) {
-        FragmentCompat.requestPermissions(this, permissions, requestCode);
+        ActivityCompat.requestPermissions(this, permissions, requestCode);
     }
 
     @Override
     public final RouterType getRouterType() {
-        return RouterType.FRAGMENT;
+        return RouterType.ACTIVITY;
     }
 
     @Override
     public final void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        mRootPresenter.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        mRootPresenter.onRequestPermissionsResult(requestCode,permissions, grantResults);
     }
 
     @Override
-    public final void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected final void onActivityResult(int requestCode, int resultCode, Intent data) {
         mRootPresenter.onActivityResult(requestCode, resultCode, data);
     }
 
-    public final boolean onBackPressed() {
-        return mRootPresenter.onBackPressed();
+    @Override
+    public final void onBackPressed() {
+        boolean handled=false;
+        for (PureFragment visibleFragment:mVisibleFragmentSet){
+            if (visibleFragment.onBackPressed()){
+                handled=true;
+            }
+        }
+        if (!handled&&!mRootPresenter.onBackPressed()){
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -167,13 +152,25 @@ public abstract class PureFragment<P extends PureRootPresenter> extends Fragment
         mRootPresenter.onConfigurationChanged(newConfig);
     }
 
-    @Override
-    public void clearFragmentBackStackImmediate() {
-        FragmentManager manager = getChildFragmentManager();
-        if (manager.getBackStackEntryCount() > 0) {
+    void addToVisibleSet(PureFragment pureFragment){
+        mVisibleFragmentSet.add(pureFragment);
+    }
+
+    void removeFromVisibleSet(PureFragment pureFragment){
+        mVisibleFragmentSet.remove(pureFragment);
+    }
+
+    public void clearFragmentBackStackImmediate(){
+        FragmentManager manager = getFragmentManager();
+        if (manager.getBackStackEntryCount()>0){
             FragmentManager.BackStackEntry first = manager.getBackStackEntryAt(0);
             manager.popBackStackImmediate(first.getId(), FragmentManager.POP_BACK_STACK_INCLUSIVE);
         }
+    }
+
+    @Override
+    public boolean shouldKeepWhenBackground(int what) {
+        return true;
     }
 
 }
--- pure-not-compact-app/src/main/java/com/sxenon/pure/core/router/app/PureFragment.java
+++ pure-not-compact-app/src/main/java/com/sxenon/pure/core/router/app/PureFragment.java
@@ -14,71 +14,101 @@
  * limitations under the License.
  */
 
-package com.sxenon.pure.core.app;
+package com.sxenon.pure.core.router.app;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
+import android.support.v13.app.FragmentCompat;
+import android.view.View;
 
 import com.sxenon.pure.core.Event;
-import com.sxenon.pure.core.global.GlobalContext;
-import com.sxenon.pure.core.router.IActivity;
 import com.sxenon.pure.core.router.IRouter;
 import com.sxenon.pure.core.router.PureRootPresenter;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
- * To be the purest wrapper for Activity
- * Attention:Those activities which call finish in onCreate() should not inherit it.
+ * 做最纯净的Fragment二次封装
  * Created by Sui on 2016/11/21.
  */
 
-public abstract class PureActivity<P extends PureRootPresenter> extends Activity implements IActivity<P> {
+public abstract class PureFragment<P extends PureRootPresenter> extends Fragment implements IRouter<P> {
+    private List<Event> mSavedEventList;
     private P mRootPresenter;
-    private final Set<PureFragment> mVisibleFragmentSet=new HashSet<>();
+    private boolean mViewCreated;
+    private boolean shouldInitRootPresenter;
+    /**
+     * 真正的对用户可见的状态！！！
+     */
+    private boolean mVisible;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mRootPresenter= bindVisitor();
-        mRootPresenter.onCreate(savedInstanceState==null?null:GlobalContext.INSTANCE.savedEventList);
-        GlobalContext.INSTANCE.activityHistoryManager.add(this);
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mRootPresenter = bindVisitor();
+        mViewCreated = true;
     }
 
     @Override
-    protected void onResume() {
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initRootPresenterIfNeeded();
+    }
+
+    @Override
+    public void onResume() {
         super.onResume();
         mRootPresenter.onResume();
     }
 
     @Override
-    protected void onPause() {
+    public void onPause() {
         super.onPause();
         mRootPresenter.onPause();
     }
 
     @Override
-    protected void onStop() {
+    public void onStop() {
         super.onStop();
         mRootPresenter.onStop();
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    public void onDestroyView() {
+        super.onDestroyView();
         //noinspection unchecked
         saveEventList(mRootPresenter.getEventForSave());
         mRootPresenter.onDestroy();
-        GlobalContext.INSTANCE.activityHistoryManager.remove(this);
+        mViewCreated = false;
+        shouldInitRootPresenter = true;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        mVisible = mViewCreated && isVisibleToUser;
+        initRootPresenterIfNeeded();
+        PureActivity activity = (PureActivity) getActivity();
+        if (mVisible) {
+            activity.addToVisibleSet(this);
+        } else {
+            if (activity != null) {
+                activity.removeFromVisibleSet(this);
+            }
+        }
+    }
+
+    private void initRootPresenterIfNeeded() {
+        if (mVisible && shouldInitRootPresenter) {
+            mRootPresenter.onCreate(mSavedEventList);
+            shouldInitRootPresenter = false;
+        }
     }
 
     @Override
@@ -92,12 +122,6 @@ public abstract class PureActivity<P extends PureRootPresenter> extends Activity
         return this;
     }
 
-    @NonNull
-    @Override
-    public Context getContext() {
-        return this;
-    }
-
     @Override
     public P getPresenter() {
         return mRootPresenter;
@@ -105,45 +129,36 @@ public abstract class PureActivity<P extends PureRootPresenter> extends Activity
 
     @Override
     public void saveEventList(List<Event> eventList) {
-        GlobalContext.INSTANCE.savedEventList=eventList;
+        mSavedEventList = eventList;
     }
 
     @Override
     public Activity getActivityCompact() {
-        return this;
+        return getActivity();
     }
 
     @Override
     public void requestPermissionsCompact(@NonNull String[] permissions, int requestCode) {
-        ActivityCompat.requestPermissions(this, permissions, requestCode);
+        FragmentCompat.requestPermissions(this, permissions, requestCode);
     }
 
     @Override
     public final RouterType getRouterType() {
-        return RouterType.ACTIVITY;
+        return RouterType.FRAGMENT;
     }
 
     @Override
     public final void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        mRootPresenter.onRequestPermissionsResult(requestCode,permissions, grantResults);
+        mRootPresenter.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
-    protected final void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public final void onActivityResult(int requestCode, int resultCode, Intent data) {
         mRootPresenter.onActivityResult(requestCode, resultCode, data);
     }
 
-    @Override
-    public final void onBackPressed() {
-        boolean handled=false;
-        for (PureFragment visibleFragment:mVisibleFragmentSet){
-            if (visibleFragment.onBackPressed()){
-                handled=true;
-            }
-        }
-        if (!handled&&!mRootPresenter.onBackPressed()){
-            super.onBackPressed();
-        }
+    public final boolean onBackPressed() {
+        return mRootPresenter.onBackPressed();
     }
 
     @Override
@@ -152,25 +167,13 @@ public abstract class PureActivity<P extends PureRootPresenter> extends Activity
         mRootPresenter.onConfigurationChanged(newConfig);
     }
 
-    void addToVisibleSet(PureFragment pureFragment){
-        mVisibleFragmentSet.add(pureFragment);
-    }
-
-    void removeFromVisibleSet(PureFragment pureFragment){
-        mVisibleFragmentSet.remove(pureFragment);
-    }
-
-    public void clearFragmentBackStackImmediate(){
-        FragmentManager manager = getFragmentManager();
-        if (manager.getBackStackEntryCount()>0){
+    @Override
+    public void clearFragmentBackStackImmediate() {
+        FragmentManager manager = getChildFragmentManager();
+        if (manager.getBackStackEntryCount() > 0) {
             FragmentManager.BackStackEntry first = manager.getBackStackEntryAt(0);
             manager.popBackStackImmediate(first.getId(), FragmentManager.POP_BACK_STACK_INCLUSIVE);
         }
-    }
-
-    @Override
-    public boolean shouldKeepWhenBackground(int what) {
-        return true;
     }
 
 }
