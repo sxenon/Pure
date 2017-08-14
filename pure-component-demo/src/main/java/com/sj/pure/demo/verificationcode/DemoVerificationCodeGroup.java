511ef3a1016558c5cf782038d95beb6b0109a7ac
sxenon <suiisagenius@gmail.com>
2017-08-14 21:18:33 +0800

    完善DemoVerificationCodeGroup

--- pure-component-demo/src/main/java/com/sj/pure/demo/verificationcode/DemoVerificationCodeGroup.java
+++ pure-component-demo/src/main/java/com/sj/pure/demo/verificationcode/DemoVerificationCodeGroup.java
@@ -16,8 +16,11 @@
 
 package com.sj.pure.demo.verificationcode;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.provider.Settings;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -27,41 +30,53 @@ import com.sj.pure.demo.pull.R;
 import com.sxenon.pure.core.ApiException;
 import com.sxenon.pure.core.mvp.IView;
 
+import rx.functions.Action1;
+
 /**
  * Demo for VerificationCodeGroup
  * Created by Sui on 2017/8/14.
  */
 
-public class DemoVerificationCodeGroup extends RxVerificationCodeGroup<RequestBean,ResponseBean> {
-    private static final String TAG="Demo";
+public class DemoVerificationCodeGroup extends RxVerificationCodeGroup<RequestBean, ResponseBean> {
+    private static final String TAG = "Demo";
     private final IView mContainer;
+    private final Button mCodeBtn;
+    private final TextView mCountDownTv;
 
-    public DemoVerificationCodeGroup(final IView container, int secondsInFuture, final Button codeButton, final TextView countDownText){
+    public DemoVerificationCodeGroup(final IView container, int secondsInFuture, final Button codeBtn, final TextView countDownTv, final Action1<View> requestCodeAction) {
         super(container.getRouter(), secondsInFuture, new CountDownListener() {
             @Override
             public void onStart() {
-                codeButton.setEnabled(false);//在发送数据的时候设置为不能点击
-                codeButton.setBackgroundColor(Color.GRAY);//背景色设为灰色
+                codeBtn.setEnabled(false);//在发送数据的时候设置为不能点击
+                codeBtn.setBackgroundColor(Color.GRAY);//背景色设为灰色
             }
 
             @Override
             public void onTick(long secondsUntilFinished) {
-                countDownText.setText(String.valueOf(secondsUntilFinished));
+                countDownTv.setText(String.valueOf(secondsUntilFinished));
             }
 
             @Override
             public void onFinish() {
-                countDownText.setText(container.getContext().getResources().getString(R.string.app_name));//随便填的，编译通过就行
-                codeButton.setEnabled(true);
-                codeButton.setBackgroundColor(Color.parseColor("#f97e7e"));
+                countDownTv.setText(container.getContext().getResources().getString(R.string.app_name));//随便填的，编译通过就行
+                codeBtn.setEnabled(true);
+                codeBtn.setBackgroundColor(Color.parseColor("#f97e7e"));
             }
         });
-        mContainer=container;
+        codeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestCodeAction.call(v);
+            }
+        });
+        mContainer = container;
+        mCodeBtn = codeBtn;
+        mCountDownTv = countDownTv;
     }
 
     @Override
     public void onSubmitFailed(ResponseBean result) {
-        Log.i(TAG,"onSubmitFailed,result="+result.toString());
+        Log.i(TAG, "onSubmitFailed,result=" + result.toString());
     }
 
     @Override
@@ -71,15 +86,15 @@ public class DemoVerificationCodeGroup extends RxVerificationCodeGroup<RequestBe
 
     @Override
     public void onCancel() {
-        doSomething();
-    }
 
-    private void doSomething(){
-        Log.i(TAG,mContainer.toString());
     }
 
     @Override
     public void onException(ApiException exception) {
-
+        mCountDownTv.setText(exception.getExceptionMsg());
+        mCodeBtn.setEnabled(true);
+        mCodeBtn.setBackgroundColor(Color.parseColor("#f97e7e"));
+        //Suppose it is a network exception;
+        mContainer.getRouter().startActivityForResult(new Intent(Settings.ACTION_WIRELESS_SETTINGS),1);
     }
 }
